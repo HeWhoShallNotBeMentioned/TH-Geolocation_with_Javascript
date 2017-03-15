6e2cb54ddf44a967baa766645962d50def94409d
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-14 21:28:52 -0700

    add 3  more errors including default.

--- geolocation.js
+++ geolocation.js
@@ -31,6 +31,15 @@ function gotError (error) {
     case error.PERMISSION_DENIED:
       message = "You need to give permission for us to have your location to calculate distances.";
       break;
+    case error.POSITION_UNAVAILABLE:
+      message = "Something went wrong obtaining the location from your device. Please refresh the page.";
+      break;
+    case error.TIMEOUT:
+      message = "The connect timed out.";
+      break;
+    default:
+      message = "An unknown error has occured please refresh the page.";
+      break;
   }
   displayError(message);
 }
